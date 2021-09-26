37c75cf008defa793d3477c65b01d1b6e468e531
inish <inish23@gmail.com>
2021-09-26 09:07:08 +0530

    completed

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useParams } from "react-router";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "../useFetch";
 
 const BlogDetails = () => {
@@ -8,7 +7,16 @@ const BlogDetails = () => {
     data: blog,
     error,
     isPending,
-  } = useFetch("http://localhost:8000/blogs" + id);
+  } = useFetch("http://localhost:8000/blogs/" + id);
+  const history = useHistory();
+
+  const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      history.push("/");
+    });
+  };
 
   return (
     <div className="blog-details">
@@ -17,8 +25,9 @@ const BlogDetails = () => {
       {blog && (
         <article>
           <h2>{blog.title}</h2>
-          <p>written by: {blog.author}</p>
+          <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>delete</button>
         </article>
       )}
     </div>
